b6bd776be0fbc9404d6c237d8c8047c463f555a6
Code With_Dipak <140307138+Code-With-Dipak@users.noreply.github.com>
2023-09-16 10:46:37 +0530

    Create tables .c

---
+++ tables .c
@@ -0,0 +1,32 @@
+// table  1 to 300 //
+
+#include <stdio.h>
+int main()
+{
+	int a,table,i;
+	printf("enter table no:-");
+	scanf("%d",&a);
+	{
+	  for(i=1;i<=10;i++)	
+	{
+	table=i*a;
+	printf("%d\n",table);
+}
+	return 0;
+}
+}
+
+
+//**************OUTPUT***********//
+
+enter table no:-2
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
